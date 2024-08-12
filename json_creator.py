0f600b56100ef8c25397e3f1853da57f4172056f
MHTpro <mht.pro1381@gmail.com>
2024-08-12 05:42:56 -0700

    import json and then create the data

---
+++ json_creator.py
@@ -0,0 +1,9 @@
+# imports
+import json
+
+# create a data with dictionary
+data = {
+    'name' : 'Mohammad hasan',
+    'age' : 22,
+    'skills': ['flutter','python','c#','unity','blender','photoshop']
+}
